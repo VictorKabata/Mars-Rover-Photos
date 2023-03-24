20e49a1c3ac815f603d327a5cfcd2490c775624b
Victor Kabata <39780120+VictorKabata@users.noreply.github.com>
2023-03-24 15:59:19 +0300

    Migrate domain build script to kts

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -1,9 +1,13 @@
 plugins {
-    id 'java-library'
-    id 'org.jetbrains.kotlin.jvm'
+    // id ("java-library")
+    alias(libs.plugins.kotlin.jvm)
 }
 
 java {
     sourceCompatibility = JavaVersion.VERSION_1_7
     targetCompatibility = JavaVersion.VERSION_1_7
+}
+
+dependencies{
+
 }
