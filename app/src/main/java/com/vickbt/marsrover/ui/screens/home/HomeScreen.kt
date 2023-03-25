1a42ba0f56c950ae5fa2600a7965d21afee48471
Victor Kabata <39780120+VictorKabata@users.noreply.github.com>
2023-03-25 04:33:49 +0300

    Set home UI

--- app/src/main/java/com/vickbt/marsrover/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/com/vickbt/marsrover/ui/screens/home/HomeScreen.kt
@@ -2,8 +2,8 @@
 
 package com.vickbt.marsrover.ui.screens.home
 
-import android.widget.Toast
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
@@ -11,11 +11,14 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.paging.compose.collectAsLazyPagingItems
 import com.vickbt.marsrover.ui.components.HomeTopAppBar
@@ -28,41 +31,46 @@ fun HomeScreen(viewModel: HomeViewModel = getViewModel()) {
     val homeUiState = viewModel.homeUiState.collectAsState().value
 
     val pagedPhotos = homeUiState.data?.collectAsLazyPagingItems()
-    val context = LocalContext.current
 
     Scaffold(
         modifier = Modifier.fillMaxSize(),
-        topBar = {
-            HomeTopAppBar(
-                onFilterClicked = {
-                    viewModel.filterRover(roverName = it)
-                    Toast.makeText(context, "Clicked: $it", Toast.LENGTH_LONG).show()
-                }
-            )
-        },
+        topBar = { HomeTopAppBar(onFilterClicked = { viewModel.filterRover(roverName = it) }) },
     ) { scaffoldPadding ->
 
-        LazyVerticalGrid(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(PaddingValues(vertical = 8.dp, horizontal = 16.dp)),
-            columns = GridCells.Fixed(2),
-            verticalArrangement = Arrangement.spacedBy(8.dp),
-            horizontalArrangement = Arrangement.spacedBy(8.dp)
-        ) {
+        Box(modifier = Modifier.fillMaxSize()) {
+            if (homeUiState.isLoading) {
+                CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
+            } else if (!homeUiState.error.isNullOrEmpty()) {
+                Text(
+                    modifier = Modifier.align(Alignment.Center),
+                    text = "Error\n${homeUiState.error}",
+                    textAlign = TextAlign.Center
+                )
+            } else {
+                LazyVerticalGrid(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(PaddingValues(vertical = 8.dp, horizontal = 16.dp))
+                        .align(Alignment.Center),
+                    columns = GridCells.Fixed(2),
+                    verticalArrangement = Arrangement.spacedBy(8.dp),
+                    horizontalArrangement = Arrangement.spacedBy(8.dp)
+                ) {
 
-            pagedPhotos?.let {
-                items(it.itemCount) { index ->
-                    pagedPhotos[index]?.let { photo ->
-                        PhotoCard(
-                            modifier = Modifier.height(250.dp),
-                            photo = photo,
-                            onClickPhoto = {}
-                        )
+                    pagedPhotos?.let {
+                        items(it.itemCount) { index ->
+                            pagedPhotos[index]?.let { photo ->
+                                PhotoCard(
+                                    modifier = Modifier.height(250.dp),
+                                    photo = photo,
+                                    onClickPhoto = {}
+                                )
+                            }
+                        }
                     }
+
                 }
             }
-
         }
 
     }
--- app/src/main/java/com/vickbt/marsrover/ui/screens/home/HomeViewModel.kt
+++ app/src/main/java/com/vickbt/marsrover/ui/screens/home/HomeViewModel.kt
@@ -1,6 +1,5 @@
 package com.vickbt.marsrover.ui.screens.home
 
-import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.paging.cachedIn
@@ -17,7 +16,7 @@ class HomeViewModel constructor(private val marsPhotosRepository: MarsPhotosRepo
     private val _homeUiState = MutableStateFlow(HomeUiState())
     val homeUiState = _homeUiState.asStateFlow()
 
-    private val _roverName = MutableStateFlow<String?>(null)
+    private val roverName = MutableStateFlow<String>("curiosity")
 
 
     init {
@@ -25,18 +24,16 @@ class HomeViewModel constructor(private val marsPhotosRepository: MarsPhotosRepo
     }
 
     private fun fetchMarsPhotos() = viewModelScope.launch {
-        _roverName.collect {
-            Log.e("VicKbt", "Filter: $it")
-            val response =
-                marsPhotosRepository.fetchMarsPhotos(roverName = _roverName.value ?: "curiosity")
-                    .cachedIn(viewModelScope)
+        roverName.collect {
+            val response = marsPhotosRepository.fetchMarsPhotos(roverName = roverName.value)
+                .cachedIn(viewModelScope)
 
             _homeUiState.update { it.copy(data = response, isLoading = false) }
         }
     }
 
     fun filterRover(roverName: String) {
-        _roverName.value = roverName
+        this.roverName.value = roverName
     }
 
 }
