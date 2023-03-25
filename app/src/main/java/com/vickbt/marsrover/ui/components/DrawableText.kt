5ce18648f52680fd57a793252eb432f49fb82b0d
Victor Kabata <39780120+VictorKabata@users.noreply.github.com>
2023-03-25 21:09:47 +0300

    Update drawable text component

--- app/src/main/java/com/vickbt/marsrover/ui/components/DrawableText.kt
+++ app/src/main/java/com/vickbt/marsrover/ui/components/DrawableText.kt
@@ -7,18 +7,28 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 @Composable
-fun DrawableText(modifier: Modifier = Modifier, @DrawableRes imageResource: Int, text: String) {
+fun DrawableText(
+    modifier: Modifier = Modifier,
+    @DrawableRes imageResource: Int,
+    text: String,
+    fontWeight: FontWeight = FontWeight.Normal,
+    fontSize: TextUnit = 16.sp,
+    color: Color = MaterialTheme.colors.onSurface
+) {
     Row(
         modifier = modifier
             .fillMaxWidth()
@@ -36,10 +46,11 @@ fun DrawableText(modifier: Modifier = Modifier, @DrawableRes imageResource: Int,
         Text(
             modifier = Modifier,
             text = text,
-            fontSize = 16.sp,
-            fontWeight = FontWeight.Black,
+            fontSize = fontSize,
+            fontWeight = fontWeight,
             overflow = TextOverflow.Ellipsis,
             maxLines = 1,
+            color = color
         )
     }
 }
--- app/src/main/java/com/vickbt/marsrover/ui/screens/details/DetailsScreen.kt
+++ app/src/main/java/com/vickbt/marsrover/ui/screens/details/DetailsScreen.kt
@@ -2,13 +2,20 @@ package com.vickbt.marsrover.ui.screens.details
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -28,6 +35,9 @@ import androidx.navigation.NavController
 import coil.compose.ImagePainter
 import coil.compose.rememberImagePainter
 import com.vickbt.domain.models.Photo
+import com.vickbt.domain.utils.toDateFormat
+import com.vickbt.marsrover.R
+import com.vickbt.marsrover.ui.components.DrawableText
 import com.vickbt.marsrover.utils.generateImagePalette
 
 @Composable
@@ -70,6 +80,32 @@ fun DetailsScreen(navController: NavController, photo: Photo) {
         )
         //endregion
 
+        //region TopAppBar
+        TopAppBar(modifier = Modifier
+            .fillMaxWidth()
+            .align(Alignment.TopCenter), title = {
+            /*photo.rover?.name?.let {
+                Text(
+                    text = it,
+                    overflow = TextOverflow.Ellipsis,
+                    maxLines = 1,
+                    color = dominantTextColor,
+                    fontWeight = FontWeight.ExtraBold,
+                    fontSize = 28.sp
+                )
+            }*/
+        }, navigationIcon = {
+            IconButton(onClick = { navController.navigateUp() }) {
+                Icon(
+                    imageVector = Icons.Rounded.ArrowBack,
+                    contentDescription = "Back",
+                    tint = dominantTextColor
+                )
+            }
+        }, backgroundColor = Color.Transparent, elevation = 0.dp
+        )
+        //endregion
+
         //region Fading Edge
         Box(
             modifier = Modifier
@@ -78,8 +114,7 @@ fun DetailsScreen(navController: NavController, photo: Photo) {
                 .background(
                     Brush.verticalGradient(
                         listOf(
-                            Color.Transparent,
-                            dominantColor
+                            Color.Transparent, dominantColor
                         )
                     )
                 )
@@ -87,22 +122,65 @@ fun DetailsScreen(navController: NavController, photo: Photo) {
         )
         //endregion
 
-        //region Rover Name
-        photo.rover?.name?.let {
-            Text(
-                modifier = Modifier
-                    .padding(vertical = 12.dp, horizontal = 16.dp)
-                    .align(Alignment.TopStart),
-                text = it,
-                fontSize = 40.sp,
-                fontWeight = FontWeight.Black,
-                overflow = TextOverflow.Ellipsis,
-                maxLines = 2,
-                color = dominantTextColor
-            )
+
+        //region
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentHeight()
+                .align(Alignment.BottomStart),
+            horizontalAlignment = Alignment.Start,
+            verticalArrangement = Arrangement.SpaceBetween
+        ) {
+
+            photo.rover?.name?.let {
+                DrawableText(
+                    imageResource = R.drawable.ic_rover,
+                    text = "Rover Name: $it",
+                    color = dominantTextColor,
+                    fontWeight = FontWeight.ExtraBold,
+                    fontSize = 28.sp
+                )
+            }
+
+            photo.rover?.launchDate?.let {
+                Text(
+                    text = "Launch Date: ${it.toDateFormat()}",
+                    overflow = TextOverflow.Ellipsis,
+                    maxLines = 1,
+                    color = dominantTextColor,
+                    fontWeight = FontWeight.ExtraBold,
+                    fontSize = 28.sp
+                )
+            }
+
+            photo.rover?.landingDate?.let {
+                Text(
+                    text = "Landing Date: ${it.toDateFormat()}",
+                    overflow = TextOverflow.Ellipsis,
+                    maxLines = 1,
+                    color = dominantTextColor,
+                    fontWeight = FontWeight.ExtraBold,
+                    fontSize = 28.sp
+                )
+            }
+
+
+            photo.rover?.status?.let {
+                Text(
+                    text = "Status: $it",
+                    overflow = TextOverflow.Ellipsis,
+                    maxLines = 1,
+                    color = dominantTextColor,
+                    fontWeight = FontWeight.ExtraBold,
+                    fontSize = 28.sp
+                )
+            }
+
         }
         //endregion
 
     }
+
 
 }
